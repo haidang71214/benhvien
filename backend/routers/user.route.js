2efa028a0cace88e2de790a41bc5f9c2ebe59b2e
lvt302000 <hn989422@gmail.com>
2025-06-11 20:18:26 +0700

    test: thử đẩy code

--- backend/routers/user.route.js
+++ backend/routers/user.route.js
@@ -1,17 +1,31 @@
-import express from 'express';
-import { middlewareTokenAsyncKey } from '../config/jwt.js';
-import { createUser, deleteUser, detailSelf, getAlluser, getDetailUser, searchDoctors, updateUser } from '../controllers/admin.controller.js';
-import { uploadCloud } from '../config/uploadCloud.js';
+import express from "express";
+import { middlewareTokenAsyncKey } from "../config/jwt.js";
+import {
+  createUser,
+  deleteUser,
+  detailSelf,
+  getAllDoctors,
+  getAlluser,
+  getDetailUser,
+  searchDoctors,
+  updateUser,
+} from "../controllers/admin.controller.js";
+import { uploadCloud } from "../config/uploadCloud.js";
 
 const userRouter = express.Router();
-userRouter.post('/createUser',middlewareTokenAsyncKey,createUser); // create new user with admin rol
-userRouter.post('/updateUser/:id',middlewareTokenAsyncKey,uploadCloud.single('img'),updateUser); // update user with admin role 
-userRouter.delete('/deleteUser/:id',middlewareTokenAsyncKey,deleteUser);// delete admin
-userRouter.get('/getAllUser',middlewareTokenAsyncKey,getAlluser) // get all
+userRouter.post("/createUser", middlewareTokenAsyncKey, createUser); // create new user with admin rol
+userRouter.post(
+  "/updateUser/:id",
+  middlewareTokenAsyncKey,
+  uploadCloud.single("img"),
+  updateUser
+); // update user with admin role
+userRouter.delete("/deleteUser/:id", middlewareTokenAsyncKey, deleteUser); // delete admin
+userRouter.get("/getAllUser", middlewareTokenAsyncKey, getAlluser); // get all
 // cần check admin
-userRouter.get('/getDetailUser/:id',middlewareTokenAsyncKey,getDetailUser) // get detail user by id
-userRouter.get('/getDetailMySelf',middlewareTokenAsyncKey,detailSelf) // get my information
-userRouter.get('/search',searchDoctors)// searchDoctor
+userRouter.get("/getAllDoctors", getAllDoctors);
+userRouter.get("/getDetailUser/:id", middlewareTokenAsyncKey, getDetailUser); // get detail user by id
+userRouter.get("/getDetailMySelf", middlewareTokenAsyncKey, detailSelf); // get my information
+userRouter.get("/search", searchDoctors); // searchDoctor
 
-
-export default userRouter; 
+export default userRouter;
--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const DoctorCard = ({ doctor }) => {
   const renderStars = (rating) => (
     <div className="flex items-center gap-1">
